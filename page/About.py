3f1e0402a55f23bfc766305173616249575202e0
puthipongyomabut <puthipong.yom@gmail.com>
2024-11-14 17:00:22 +0700

    commit About us

--- page/About.py
+++ page/About.py
@@ -1,43 +1,53 @@
 import streamlit as st
-
-
-def load_css():
-    with open('style.css') as f:
-        st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
+import base64
+from pathlib import Path
+from PIL import Image
+from io import BytesIO
+def convert_image(img):
+    image = Image.open(img).convert("RGB")
+    buffered = BytesIO()
+    image.save(buffered, format="JPEG")
+    img_str = base64.b64encode(buffered.getvalue()).decode()
+    return img_str
 
 def show_about():
     load_css()
     st.markdown('<div style="margin:50px;"><h1 class="center-header">Team Members</h1></div>', unsafe_allow_html=True)
-
+    img_nana = convert_image('src/Nana.jpg')
+    img_pleum = convert_image('src/Pleum.jpg')
+    img_phat = convert_image('src/Phat.jpg')
+    img_ter = convert_image('src/Ter.jpg')
     st.markdown(
-    """
+    f"""
     <div class="about-container">
         <div>
             <div class="circle-image">
-                <img src="https://staticg.sportskeeda.com/editor/2023/02/394a3-16769313907566-1920.jpg" alt="Nipatsa Chainiwattana">
+                <img src="data:image/jpeg;base64,{img_nana}" alt="Nipatsa Chainiwattana">
             </div>
             <div class="name-text">Nipatsa Chainiwattana</div>
         </div>
         <div>
             <div class="circle-image">
-                <img src="https://staticg.sportskeeda.com/editor/2023/02/394a3-16769313907566-1920.jpg" alt="Puthipong Yomabut">
+                <img src="data:image/jpeg;base64,{img_ter}" alt="Puthipong Yomabut">
             </div>
             <div class="name-text">Puthipong Yomabut</div>
         </div>
         <div>
             <div class="circle-image">
-                <img src="https://staticg.sportskeeda.com/editor/2023/02/394a3-16769313907566-1920.jpg" alt="Patiharn Kamenkit">
+                <img src="data:image/jpeg;base64,{img_pleum}" alt="Patiharn Kamenkit">
             </div>
             <div class="name-text">Patiharn Kamenkit</div>
         </div>
         <div>
             <div class="circle-image">
-                <img src="https://staticg.sportskeeda.com/editor/2023/02/394a3-16769313907566-1920.jpg" alt="Phattaradanai Sornsawang">
+                <img src="data:image/jpeg;base64,{img_phat}" alt="Phattaradanai Sornsawang">
             </div>
             <div class="name-text">Phattaradanai Sornsawang</div>
         </div>
     </div>
     """,
-    unsafe_allow_html=True
-)
+    unsafe_allow_html=True)
 
+def load_css():
+    with open('style.css') as f:
+        st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
