633d711e0fd3e6bd94ef94236289171c67128e5f
puthipongyomabut <puthipong.yom@gmail.com>
2024-11-19 14:33:44 +0700

    Update graph with new visual

--- page/Visual.py
+++ page/Visual.py
@@ -4,12 +4,41 @@ import streamlit as st
 import plotly.graph_objects as go
 import base64
 import matplotlib.pyplot as plt
+import numpy as np
+from tensorflow.keras.applications.resnet50 import preprocess_input
 from page.Upload import predict
 from page.Upload import load_model
 from PIL import Image
 from io import BytesIO
 
-def predict_visual(img):
+def load_css():
+    with open('style.css') as f:
+        st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
+        
+def predict_visual(image_file, model):
+    if image_file is not None:
+        if model is None:
+            st.error("No model selected or model loading failed.")
+            return
+        
+        image = Image.open(image_file).convert("RGB")
+        
+        img_array = np.array(image.resize((224, 224))) 
+        img_array = np.expand_dims(img_array, axis=0)  
+        img_array = preprocess_input(img_array)
+        
+        prediction = model.predict(img_array)
+        predicted_class = np.argmax(prediction, axis=-1)
+
+        if predicted_class[0] == 0:
+            st.write("This picture is FAKE")
+        else:
+            st.write("This picture is REAL")
+        
+        st.write("Fake Probility : ",np.round(prediction[0][0] * 100, 2),'%')
+        st.write("Real Probility : ",np.round(prediction[0][1] * 100, 2),'%')
+
+def print_image_visual(img):
     image = Image.open(img).convert("RGB")
         
     buffered = BytesIO()
@@ -24,19 +53,56 @@ def predict_visual(img):
             """,
             unsafe_allow_html=True
         )
-    st.markdown('<h3> Model 1 </h3>', unsafe_allow_html=True)
-    predict(img, load_model('Model 1'))
-    st.markdown('<h3> Model 2 </h3>', unsafe_allow_html=True)
-    predict(img, load_model('Model 2'))
-
-def load_css():
-    with open('style.css') as f:
-        st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
 
 def get_random_image(image_folder):
     all_images = os.listdir(image_folder)
     image_files = [f for f in all_images if f.endswith(('.png', '.jpg', '.jpeg', '.bmp', '.gif'))]
     return os.path.join(image_folder, random.choice(image_files)) if image_files else None
+
+
+def show_visual():
+    load_css()
+
+    fake_folder = "src/fake"
+    real_folder = "src/real"
+
+    st.markdown('<h1 class="center-header">AI Image Example</h1>', unsafe_allow_html=True)
+
+    col1, col2, col3 = st.columns(3)
+
+    with col1:
+        image_path = get_random_image(fake_folder)
+        if image_path:
+            print_image_visual(image_path)
+        else:
+            st.write("No images found in fake folder.")
+    with col3:
+        st.markdown('<h4>Model 1</h4>', unsafe_allow_html=True)
+        predict(image_path, load_model('Model 1'))
+        st.markdown('<h4>Model 2</h4>', unsafe_allow_html=True)
+        predict(image_path, load_model('Model 2'))
+        st.markdown('<h4>Model 3</h4>', unsafe_allow_html=True)
+        predict(image_path, load_model('Model 2'))
+        
+    st.markdown('<h1 class="center-header">Real Image Example</h1>', unsafe_allow_html=True)
+
+    col4, col5, col6 = st.columns(3)
+    with col4:
+        image_path = get_random_image(real_folder)
+        if image_path:
+            print_image_visual(image_path)
+        else:
+            st.write("No images found in fake folder.")
+    with col6:
+        st.markdown('<h4>Model 1</h4>', unsafe_allow_html=True)
+        predict(image_path, load_model('Model 1'))
+        st.markdown('<h4>Model 2</h4>', unsafe_allow_html=True)
+        predict(image_path, load_model('Model 2'))
+        st.markdown('<h4>Model 3</h4>', unsafe_allow_html=True)
+        predict(image_path, load_model('Model 2'))
+        
+    visualize_graph()
+
 
 def visualize_graph():
     labels = ['AI painting pictures', 'REAL painting pictures']
@@ -240,33 +306,3 @@ def visualize_graph():
     with col2:
         st.plotly_chart(loss_fig, use_container_width=True)
    
-
-def show_visual():
-    load_css()
-
-    fake_folder = "src/fake"
-    real_folder = "src/real"
-
-    st.markdown('<h1 class="center-header">AI Image Example</h1>', unsafe_allow_html=True)
-
-    col1, col2, col3, col4 = st.columns(4)
-    for col in [col1, col2, col3, col4]:
-        with col:
-            image_path = get_random_image(fake_folder)
-            if image_path:
-                predict_visual(image_path)
-            else:
-                st.write("No images found in fake folder.")
-
-    st.markdown('<h1 class="center-header">Real Image Example</h1>', unsafe_allow_html=True)
-
-    col5, col6, col7, col8 = st.columns(4)
-    for col in [col5, col6, col7, col8]:
-        with col:
-            image_path = get_random_image(real_folder)
-            if image_path:
-                predict_visual(image_path)
-            else:
-                st.write("No images found in real folder.")
-
-    visualize_graph()
