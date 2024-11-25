32fa34de9a48569c9a6d8db424869a6882365922
puthipongyomabut <puthipong.yom@gmail.com>
2024-11-25 19:32:54 +0700

    Final-change-3

--- page/app.py
+++ page/app.py
@@ -1,40 +1,39 @@
-# from page import Home, About, Upload, Visual
 import streamlit as st
-import pandas as pd
-import numpy as np
-import streamlit_option_menu
 from streamlit_option_menu import option_menu
-# from page.Home import show_home
-# from page.About import show_about
-# from page.Upload import show_upload
-# from page.Visual import show_visual
+
+# Import page modules
 from Home import show_home
 from About import show_about
 from Upload import show_upload
 from Visual import show_visual
 
-
-
+# Create the sidebar menu
 with st.sidebar:
     selected = option_menu(
-        menu_title = 'Main Menu',
-        options = ['Home', 'Visual', 'Upload', 'About us'],
-        icons = ['house', 'eye', 'upload', 'info-circle'],
-        menu_icon = 'cast',
-        default_index = 0
+        menu_title="Main Menu",  # Sidebar menu title
+        options=["Home", "Visual", "Upload", "About us"],  # Menu options
+        icons=["house", "eye", "upload", "info-circle"],  # Icons for the options
+        menu_icon="cast",  # Icon for the menu
+        default_index=0  # Default selected option
     )
 
+# Define a function to switch between pages
 def switch_case(option):
-    if option == 'Home':
-        return show_home()
-    elif option == 'Visual':
-        return show_visual()
-    elif option == 'Upload':
-        return show_upload()
-    else : return show_about()
+    if option == "Home":
+        show_home()
+    elif option == "Visual":
+        show_visual()
+    elif option == "Upload":
+        show_upload()
+    elif option == "About us":
+        show_about()
+    else:
+        st.error("Page not found!")  # Fallback for undefined options
 
+# Main function
 def main():
     switch_case(selected)
 
-if __name__=='__main__':
+# Entry point of the app
+if __name__ == "__main__":
     main()
